d9a7f816457b7f821e89daf00abbfd3aaf4fafde
Daniel Austin <daniel@datarepublic.com>
2020-08-14 09:05:24 +1000

    Fixing helper scripts to work with refactored Sudoku class

--- benchmarking.py
+++ benchmarking.py
@@ -1,28 +1,26 @@
 # benchmarking.py
 
-import copy
 import timeit
 import sudoku
-from sudoku import EMPTY_CELL, CAGE_SIZE, MAX_CELL_VALUE
 
 
-class BenchmarkerPuzzle(sudoku.SudokuPuzzleConstrained):
+class BenchmarkerPuzzle(sudoku.SudokuPuzzle):
     def foo(self):
         return
 
 
-def bench(function, use_class=sudoku.SudokuPuzzleConstrained, setup=""):
-    p = use_class(sudoku.SAMPLE_PUZZLES[6]["puzzle"])
+def bench(function, use_class=sudoku.SudokuPuzzle, setup=""):
+    p = use_class()
+    p.init_puzzle(sudoku.SAMPLE_PUZZLES[6]["puzzle"])
     t = timeit.timeit(f"{function}", globals={"p": p}, setup=setup)
     print(f"{use_class.__name__} - {function} \t= {t}")
     return
 
 
 # Sanity check
-p = BenchmarkerPuzzle(sudoku.SAMPLE_PUZZLES[6]["puzzle"])
+p = BenchmarkerPuzzle()
 assert [m for m in p.next_empty_cell()] == p.get_all_empty_cells()
 
 print("GET EMPTY CELLS")
 bench("p.find_empty_cell()", use_class=BenchmarkerPuzzle)
-bench("next(gen)", setup="gen = p.next_empty_cell(loop_once=False)")
-bench("p.find_empty_cell()")
+bench("p.get_all_empty_cells()")
--- play.py
+++ play.py
@@ -1,26 +1,26 @@
 # play
 
 import sudoku
-import copy
 import cProfile
 
 
 def solve_using_backtracking(puzzle):
     """
-    Attempts to solve `puzzle` using backtracking, trying legal values and testing first. Returns True if puzzle is solved, 
-    False if the current solution path is a dead-end (results in invalid puzzle). Calls itself recursively.
+    Attempts to solve `puzzle` using backtracking, trying legal values and
+    testing first. Returns True if puzzle is solved, False if the current
+    solution path is a dead-end (results in invalid puzzle).
     """
     if puzzle.num_empty_cells() <= 0:
         return True
 
-    mtGen = puzzle.next_empty_cell()
+    mtGen = puzzle.next_best_empty_cell()
     try:
         empty_cell = next(mtGen)
     except StopIteration:
         return True
 
     x, y = empty_cell[0], empty_cell[1]
-    for val in puzzle.get_possible_values(x, y):
+    for val in puzzle.get_allowed_values(x, y):
         puzzle.set(x, y, val)
         if solve_using_backtracking(puzzle):
             return True
@@ -30,8 +30,9 @@ def solve_using_backtracking(puzzle):
     return False
 
 
-def play_sudoku(i=0, use_class=sudoku.SudokuPuzzleConstrained):
-    p = use_class(sudoku.SAMPLE_PUZZLES[i]["puzzle"])
+def play_sudoku(i=0, use_class=sudoku.SudokuPuzzle):
+    p = use_class()
+    p.init_puzzle(sudoku.SAMPLE_PUZZLES[i]["puzzle"])
     print(sudoku.SAMPLE_PUZZLES[i]["label"])
     print(p)
 
@@ -39,7 +40,6 @@ def play_sudoku(i=0, use_class=sudoku.SudokuPuzzleConstrained):
     print("\nSolution:")
     print(p)
     assert p.is_solved()
-    return
 
 
 cProfile.run("play_sudoku(5)", sort="tottime")
