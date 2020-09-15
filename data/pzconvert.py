f9ebf04a82b6efb46f5c77e4e04e1d137e2f69dc
Daniel Austin <daniel@datarepublic.com>
2020-09-16 07:54:34 +1000

    Removed invalid puzzles (bad conversion)

--- data/pzconvert.py
+++ data/pzconvert.py
@@ -7,7 +7,8 @@ ALPH = '123456789ABCDEFGHIJKLMNOPQ'
 HEXA = '123456789ABCDEFGHIJKLMNOPQ'
 
 in_headrow = True
-puzzle = []
+puzzle1 = []
+puzzle2 = []
 linenum = 0
 for line in fileinput.input():
     linenum += 1
@@ -17,10 +18,13 @@ for line in fileinput.input():
 
     line = line.rstrip()
     line = line.replace('0', '.')
-    # bits = [HEXA[ALPH.index(x)] if x else '.' for x in line]
-    puzzle.append(line)
+    bits = [HEXA[ALPH.index(x)] if x else '.' for x in line.split(',')]
+    puzzle1.append(''.join(bits[0:15]))
+    puzzle1.append(''.join(bits[16:31]))
 
-    if len(puzzle) == 9:
+    if len(puzzle1) == 16:
         in_headrow = True
-        print(''.join(puzzle))
-        puzzle = []
+        print(''.join(puzzle1))
+        print(''.join(puzzle2))
+        puzzle1 = []
+        puzzle2 = []
